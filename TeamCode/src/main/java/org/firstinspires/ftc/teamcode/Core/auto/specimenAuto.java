d06915d013c1c55d88a2aecdca910ae62d6a5208
cnetz <Cameronjnetz@gmail.com>
2024-12-20 17:22:01 -0700

    naming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/auto/specimenAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/auto/specimenAuto.java
@@ -11,6 +11,7 @@ import com.arcrobotics.ftclib.command.WaitCommand;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Const;
 import org.firstinspires.ftc.teamcode.Core.Commands.drive.PathCommand;
 import org.firstinspires.ftc.teamcode.Core.Commands.subsystems.joint.SetJoint;
 import org.firstinspires.ftc.teamcode.Core.Commands.subsystems.servos.SetClaw;
@@ -23,8 +24,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 
 @Autonomous
 public class specimenAuto extends LinearOpMode {
-    public static Path[] paths = new Path[2];
-    private final Pose startPosition = new Pose(24,36, Math.toRadians(0)); //270 = South
+    public static Path[] paths = new Path[16];
     private final Pose placeSpecimenPosition1 = new Pose(36, 72, Math.toRadians(0));
     private final Pose placeSpecimenPosition2 = new Pose(36, 70, Math.toRadians(0));
     private final Pose placeSpecimenPosition3 = new Pose(36, 68, Math.toRadians(0));
@@ -39,13 +39,8 @@ public class specimenAuto extends LinearOpMode {
     private final Pose pickupSpecimenPosition = new Pose(26,24, Math.toRadians(180));
 
     public void buildPaths() {
-//        paths[0] = buildLine(
-//                startPose,
-//                scorePose,
-//                HeadingInterpolation.LINEAR
-//        );
         paths[0] = buildLine(
-                startPosition,
+                Constants.specimenStartPosition,
                 placeSpecimenPosition1,
                 HeadingInterpolation.CONSTANT
         );
@@ -75,15 +70,15 @@ public class specimenAuto extends LinearOpMode {
             telemetry.update();
         }
 
-        //robot.setStartingPose(startPosition);
-        robot.setPose(startPosition);
+        //robot.setStartingPose(Constants.specimenStartPosition);
+        robot.setPose(Constants.specimenStartPosition);
 
         CommandScheduler.getInstance().schedule(
                 new SequentialCommandGroup(
                         new ParallelCommandGroup(
                                 new PathCommand(paths[0]),
-                                new SetSlide(robot.slide, 2000),
-                                new SetJoint(robot.joint, Constants.specimenPlacePosition)
+                                new SetSlide(robot.slide, Constants.slideMaxPosition),
+                                new SetJoint(robot.joint, Constants.jointSpecimenPlacePosition)
                         ),
                         new SetClaw(robot.claw, Constants.clawOpenPosition),
                         new WaitCommand(200),
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/util/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/util/Constants.java
@@ -4,32 +4,37 @@ import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
 
 public class Constants {
 
-    /* ----------- NAMES ------------- */
+    /* ----------- NAMES ----------- */
 
+    //TODO: ADD STUFF
 
+    /* ----------- TELEOP ----------- */
 
+    //TODO: ADD STUFF
 
+    /* ----------- AUTO ----------- */
 
-    /* ----------- TELEOP ------------- */
-
-    /* ----------- AUTO ------------- */
-
-    /* --- Pinpoint Fix (remove eventaully) --- */
+    /* ----------- Pinpoint Fix (remove eventually) ----------- */
 
     public static double forwardOffsetMM_X = -120.000014;
     public static double forwardOffsetMM_Y = -135.999982;
     public static double strafeOffsetMM_X = 120.000014;
     public static double strafeOffsetMM_Y = -135.999982;
 
-    /* --- Pinpoint --- */
+    /* ----------- Pinpoint ----------- */
 
     public static double xOffsetMM = -120.000014;
     public static double yOffsetMM = -135.999982;
     public static double xOffsetInch = (-120.000014 / 25.4); // 4.72441
     public static double yOffsetInch = (-135.999982 / 25.4); //5.35433
     public static double maxPower = 1;
+
+    /* ----------- PEDRO POSITIONS ----------- */
+
     public static Pose sampleStartPosition = new Pose(7.5, 112.5, Math.toRadians(90));
     public static Pose specimenStartPosition = new Pose(7.5, 72, Math.toRadians(0));
+
+    /* ------- MOTOR SECTION ------- */
 
     /* ----------- JOINT ----------- */
 
@@ -43,7 +48,7 @@ public class Constants {
     public static double slideHighBasketPosition = 2800;
     public static double slideLowBasketPosition = 2000;
 
-    /* ----------- SERVO ------------- */
+    /* ------- SERVO SECTION ------- */
 
     /* ----------- CLAW ----------- */
     public static double clawOpenPosition = 0.55;
